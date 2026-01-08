618c23cefa632be638c70e8cda46b00786a6d55a
arcpac <antonraphaelcaballes@gmail.com>
2026-01-08 17:21:44 +1100

    Initial deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/arcpac.github.io" : "",
+  assetPrefix: isProd ? "/arcpac.github.io/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
